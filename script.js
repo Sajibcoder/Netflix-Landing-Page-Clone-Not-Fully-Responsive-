47f8fad1633aa99cab8d3a49c0be169d95a1d9a7
Sajib Das <120473050+Sajibcoder@users.noreply.github.com>
2023-02-01 17:59:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,71 @@
+const target = document.querySelector('.ans1');
+const btn = document.getElementById('q1');
+
+btn.onclick = function () {
+    if(target.style.display !== "none"){
+        target.style.display = "none";
+    }
+    else{
+        target.style.display = "block"
+    }
+}
+
+
+
+const target1 = document.querySelector('.ans2');
+const btn1 = document.getElementById('q2');
+
+btn1.onclick = function () {
+    if(target1.style.display !== "none"){
+        target1.style.display = "none";
+    }
+    else{
+        target1.style.display = "block"
+    }
+}
+
+const target2 = document.querySelector('.ans3');
+const btn2 = document.getElementById('q3');
+
+btn2.onclick = function () {
+    if(target2.style.display !== "none"){
+        target2.style.display = "none";
+    }
+    else{
+        target2.style.display = "block"
+    }
+}
+
+const target3 = document.querySelector('.ans4');
+const btn3 = document.getElementById('q4');
+
+btn3.onclick = function () {
+    if(target3.style.display !== "none"){
+        target3.style.display = "none";
+    }
+    else{
+        target3.style.display = "block"
+    }
+}
+const target4 = document.querySelector('.ans5');
+const btn4 = document.getElementById('q5');
+
+btn4.onclick = function () {
+    if(target4.style.display !== "none"){
+        target4.style.display = "none";
+    }
+    else{
+        target4.style.display = "block"
+    }
+}
+const target5 = document.querySelector('.ans6');
+const btn5 = document.getElementById('q6');
+
+btn5.onclick = function () {
+    if(target5.style.display !== "none"){
+        target5.style.display = "none";
+    }
+    else{
+        target5.style.display = "block"
+    }
+}
